3ff4a71d9440919e5f56a34bc44f8b0a3154fd7e
Nuno Marques <nmarques@equalexperts.com>
2014-08-10 02:22:15 +0100

    structural changes

--- src/main/scala/nuno/fp/chapter2/Exercise1.scala
+++
@@ -1,9 +0,0 @@
-package nuno.fp.chapter2
-
-object Exercise1 {
-  def fib(n: Long): Long = n match {
-    case a if a < 1 => 0
-    case 1 => 1
-    case _ => fib(n-1) + fib(n-2)
-  }
-}
--- src/main/scala/nuno/fp/chapter3/Exercise10.scala
+++
@@ -1,9 +0,0 @@
-package nuno.fp.chapter3
-
-object Exercise10 {
-  @annotation.tailrec
-  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
-    case Nil => z
-    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
-  }
-}
