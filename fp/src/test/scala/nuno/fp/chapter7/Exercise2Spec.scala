d4484bec538a10da40e740e25b432e7c207894e6
Nuno Marques <nmarques@equalexperts.com>
2014-09-17 17:16:08 +0100

    Some updates after looking at other peoples solutions

---
+++ fp/src/test/scala/nuno/fp/chapter7/Exercise2Spec.scala
@@ -0,0 +1,11 @@
+package nuno.fp.chapter7
+
+import org.scalatest.WordSpec
+
+class Exercise2Spec extends WordSpec {
+  "Exercise 2" should {
+    "answer question" in {
+      println("I can only envision a representation as a trait that can be specialised to different parallel strategies.")
+    }
+  }
+}
