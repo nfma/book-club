0134c500be34f9127f27b8815c30f9743d5d6b33
Paul Kelly <pkelly@equalexperts.com>
2014-09-22 11:46:24 +0100

    [PK] - Chapter 5 exercises, start on forall

--- fp/src/main/scala/paul/fp/chapter5/Exercise1.scala
+++ fp/src/main/scala/paul/fp/chapter5/Exercise1.scala
@@ -7,9 +7,9 @@ object Exercise1 {
 
   trait Stream[+A] {
 
-    def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+    def foldRight[B](z: => B)(f: (A, => B) => B): B =
       this match {
-        case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+        case Cons(h, t) => f(h(), t().foldRight(z)(f))
         case _ => z
       }
 
@@ -51,7 +51,8 @@ object Exercise1 {
       case _ => Stream()
     }
 
-    def forAll(p: A => Boolean): Boolean = sys.error("todo")
+    def forAll(p: A => Boolean): Boolean = true
+//      foldRight(false)((a, b) => p(a) || b)
 
     def startsWith[A](s: Stream[A]): Boolean = sys.error("todo")
 
--- fp/src/test/scala/paul/fp/chapter5/Chapter5Spec.scala
+++ fp/src/test/scala/paul/fp/chapter5/Chapter5Spec.scala
@@ -1,5 +1,6 @@
 package paul.fp.chapter5
 
+import paul.fp.chapter5.Exercise1.Stream
 import paul.fp.chapter5.Exercise1.{Stream, Empty}
 import org.scalatest.{Matchers, WordSpec}
 import paul.fp.chapter5.Exercise1.Stream
@@ -32,6 +33,15 @@ class Chapter5Spec extends WordSpec with Matchers {
   "Exercise 5.3 - take" should {
     "implement takeWhile(p: A => Boolean)" in {
       Stream(1, 2, 3, 4, 5).takeWhile(_ => true).toList should be(Stream(1,2,3,4,5).toList)
+    }
+  }
+
+  "Exercise 5.4 - forAll" should {
+    "implement forAll(p: A => Boolean)" in {
+      Stream() forAll {_ => true} shouldBe true
+//      Stream(1, 2) forAll {_ => false} shouldBe false
+//      Stream(1, 2) forAll {_ => true} shouldBe true
+//      Stream(2, 4, 5) forAll {_ % 2 == 0} shouldBe false
     }
   }
 
