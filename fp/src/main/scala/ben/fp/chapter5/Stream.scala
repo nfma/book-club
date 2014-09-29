f8a649fec1584a13d7108ba9a69093100bfe3ce8
Ben Letton <bletton@equalexperts.com>
2014-09-29 07:53:55 +0100

    Up to 5.13 in Streams

--- fp/src/main/scala/ben/fp/chapter5/Stream.scala
+++ fp/src/main/scala/ben/fp/chapter5/Stream.scala
@@ -15,11 +15,48 @@ object Stream {
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
+
   @tailrec
   private def take[A](s: Stream[A], n: Int, results: Stream[A]): Stream[A] = s match {
     case Empty => results
     case _ if n <= 0 => results
     case Cons(h, t) => take(t(), n - 1, Stream.cons(h(), results))
+  }
+
+  def constant[A](a:A): Stream[A] = Stream.cons(a, constant(a))
+
+  def from(a:Int): Stream[Int] = Stream.cons(a, from(a + 1))
+
+  def fibs: Stream[Int] = {
+    def sequece(a:Int, b:Int): Stream[Int] =
+      cons(a, sequece(b, a+b))
+    sequece(0,1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z) map( r => cons(r._1, unfold(r._2)(f))) getOrElse Stream.empty[A]
+
+  def append[S >: A, A](a1: Stream[A], a2: Stream[S]): Stream[S] = a1.foldRight(a2)((a,b) => Stream.cons(a, b))
+
+
+  val ones = constant(1)
+
+  def constant2[A](a:A): Stream[A] = unfold(a)(_ => Some(a, a))
+
+  def from2(a:Int): Stream[Int] = unfold(a)(i => Some(i, i+1))
+
+  def fibs2: Stream[Int] = {
+    def sequence(a:Int, b:Int): (Int, Int) = (b, a+b)
+    unfold(0,1)( a => Some(a._1, sequence(a._1,a._2)) )
+  }
+
+
+
+  @tailrec
+  private def takeWhile[A](s: Stream[A], p: A => Boolean, results: Stream[A]): Stream[A] = s match {
+    case Empty => results
+    case Cons(h, t) if !p(h()) => results
+    case Cons(h, t) if p(h()) => takeWhile(t(), p, Stream.cons(h(), results))
   }
 
   @tailrec
@@ -39,13 +76,42 @@ object Stream {
 
 trait Stream[+A] {
 
-  def take(n: Int): Stream[A] =  Stream take(this, n, Empty) reverse
+  def take(n: Int): Stream[A] = Stream take(this, n, Empty) reverse
 
   def reverse: Stream[A] = Stream reverse(this, Empty)
 
   def drop(n: Int): Stream[A] = Stream drop(this, n, Empty)
 
+  def map[B](f: A => B): Stream[B] = this match {
+    case Empty => Empty
+    case Cons(h,t) => Stream.cons(f(h()), t().map(f))
+  }
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = this match {
+    case Empty => Empty
+    case Cons(h,t) => Stream.append(f(h()), t().flatMap(f))
+  }
+
+  def filter(f: A => Boolean): Stream[A] = foldRight(Stream.empty[A])((a,b) => if(f(a)) Stream.cons(a,b) else b)
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
+
+  def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean = !exists(p(_) == false)
+
   def toList: List[A] = listify[A](this, Nil)
+
+  def takeWhile(p: A => Boolean): Stream[A] = foldRight(Stream.empty[A])(
+    (a,stream) => if (p(a)) Stream.cons(a, stream) else Stream empty
+  )
+
+  def takeWhile1(p: A => Boolean): Stream[A] = Stream takeWhile(this, p, Empty) reverse
+
+  def append[S >: A](other:Stream[S]) : Stream[S] = Stream.append[S,S](this, other)
 
   @tailrec
   private def listify[B](s: => Stream[B], results: List[B]): List[B] = s match {
@@ -56,6 +122,11 @@ trait Stream[+A] {
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
+  }
+
+  def headOption2: Option[A] = foldRight[Option[A]](None) {
+    case (a, b) => Some(a)
+    case _ => None
   }
 
 }
--- fp/src/test/scala/ben/fp/chapter5/Chapter5Spec.scala
+++ fp/src/test/scala/ben/fp/chapter5/Chapter5Spec.scala
@@ -5,28 +5,152 @@ import org.scalatest.{Matchers, WordSpec}
 
 class Chapter5Spec extends WordSpec with Matchers {
 
-  val stream: Stream[String] = Stream("a", "b", "c", "d", "e", "f")
-
+  val stringStream: Stream[String] = Stream("a", "b", "c", "d", "e", "f")
+  val numericStream: Stream[Int] = Stream(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  
   "Exercise 5.1 - Streams" should {
     "implement toList" in {
 
-      stream.toList shouldBe List("a", "b", "c", "d", "e", "f")
+      stringStream.toList shouldBe List("a", "b", "c", "d", "e", "f")
     }
   }
 
   "Exercise 5.2 - Streams" should {
     "implement take" in {
 
-      stream.take(3).toList shouldBe List("a","b","c")
+      stringStream.take(3).toList shouldBe List("a", "b", "c")
     }
-  }
 
-
-  "Exercise 5.3 - Streams" should {
     "implement drop" in {
 
-      stream.drop(3).toList shouldBe List("d", "e", "f")
+      stringStream.drop(3).toList shouldBe List("d", "e", "f")
     }
   }
 
+  "Exercise 5.3 - Streams" should {
+    "implement takeWhile" in {
+
+
+      stringStream.takeWhile1(_ => true).toList shouldBe stringStream.toList
+      val sublist: List[String] = List("a", "b", "c")
+      stringStream.takeWhile1(s => sublist.contains(s)).toList shouldBe List("a", "b", "c")
+
+    }
+  }
+
+  "Exercise 5.4 - Streams" should {
+    "implement forAll" in {
+
+      numericStream.forAll(_ < 10) shouldBe true
+      numericStream.forAll(_ > 10) shouldBe false
+      numericStream.forAll(_ < 5) shouldBe false
+    }
+  }
+
+  "Exercise 5.5 - Streams" should {
+    "implement takeWhile" in {
+
+      numericStream.takeWhile(_ < 10).toList shouldBe numericStream.toList
+      numericStream.takeWhile(_ < 5).toList shouldBe Stream(1, 2, 3, 4).toList
+      numericStream.takeWhile(_ < 0).toList shouldBe List()
+      Stream.empty[Int].takeWhile(_ < 0).toList shouldBe List()
+
+    }
+  }
+
+  "Exercise 5.6 - Streams" should {
+    "implement headOption" in {
+
+      numericStream.headOption2 shouldBe Some(1)
+      Stream().headOption2 shouldBe None
+      Stream.empty.headOption2 shouldBe None
+    }
+  }
+
+  "Exercise 5.7 - Streams" should {
+
+    "implement map" in {
+
+      Stream(1, 2, 3).map(_ + 1).toList shouldBe List(2,3,4)
+      Stream.empty[Int].map(_ + 1) shouldBe Empty
+    }
+
+    "implement flatMap" in {
+
+      Stream(1, 2).flatMap(m => stringStream.take(2).map(_ + m)).toList shouldBe List("a1","b1","a2","b2")
+      Stream(1, 2).flatMap(m => Stream.empty).toList shouldBe List()
+      Stream.empty[Int].flatMap(m => stringStream).toList shouldBe List()
+    }
+
+    "implement filter" in {
+      
+      numericStream.filter(_ % 2 == 0).toList shouldBe numericStream.toList.filter(_ % 2 == 0)
+      numericStream.filter(_ > 10).toList shouldBe Nil
+      Stream.empty[Int].filter(_ % 2 == 0) shouldBe Stream.empty
+    }
+
+    "implement append" in {
+
+      numericStream.append(numericStream).toList shouldBe numericStream.toList ++ numericStream.toList
+    }
+  }
+
+  "Exercise 5.8 - Streams" should {
+
+    "provide function constant" in {
+
+      Stream.constant(1).take(5).toList shouldBe List(1,1,1,1,1)
+      Stream.constant("a").take(5).toList shouldBe List("a","a","a","a","a")
+    }
+  }
+
+  "Exercise 5.9 - Streams" should {
+
+    "provide function from" in {
+
+      Stream.from(1).take(5).toList shouldBe List(1,2,3,4,5)
+    }
+  }
+
+  "Exercise 5.10 - Streams" should {
+
+    "provide function fibs" in {
+
+      Stream.fibs.take(10).toList shouldBe List(0,1,1,2,3,5,8,13,21,34)
+    }
+  }
+
+  "Exercise 5.11 - Streams" should {
+
+    "provide function unfold" in {
+
+      Stream.unfold(1)( p => Some(p, p+1)).take(5).toList shouldBe List(1,2,3,4,5)
+    }
+  }
+
+  "Exercise 5.12 - Streams" should {
+
+    "provide function fibs2 implemented using unfold" in {
+
+      Stream.fibs2.take(10).toList shouldBe List(0,1,1,2,3,5,8,13,21,34)
+    }
+
+    "provide function from implemented using unfold" in {
+
+      Stream.from2(0).take(10).toList shouldBe List(0,1,2,3,4,5,6,7,8,9)
+    }
+
+
+    "provide function constant implemented using unfold" in {
+
+      Stream.constant2(1).take(5).toList shouldBe List(1,1,1,1,1)
+      Stream.constant2("a").take(5).toList shouldBe List("a","a","a","a","a")
+    }
+
+
+    "provide function ones implemented using unfold" in {
+
+      Stream.ones.take(5).toList shouldBe List(1,1,1,1,1)
+    }
+  }
 }
