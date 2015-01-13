f0f63c907271eae2d9b1f5746c1d2c38a0ce86c4
Nuno Marques <nmarques@equalexperts.com>
2015-01-13 22:50:00 +0000

    updated exercise 3.13 to use functions instead of the trick that reverses a list

--- fp/src/main/scala/nuno/fp/chapter3/Exercise13.scala
+++ fp/src/main/scala/nuno/fp/chapter3/Exercise13.scala
@@ -1,9 +1,7 @@
 package nuno.fp.chapter3
 
-import nuno.fp.chapter3.Exercise12.reverse
-
 object Exercise13 {
-  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = Exercise9.foldRight(reverse(as), z) {(a, b) => f(b, a)}
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = Exercise9.foldRight(as, identity[B]) {(a, g) => b => g(f(b, a))}(z)
 
-  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = Exercise10.foldLeft(reverse(as), z) {(b, a) => f(a, b)}
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = Exercise10.foldLeft(as, identity[B]) {(g, a) => b => g(f(a, b))}(z)
 }
