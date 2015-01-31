3c15c1d93ea635dc244ff54ebc3dda80f59babcd
Nuno Marques <nmarques@equalexperts.com>
2015-01-31 11:22:42 +0000

    split the card test property into three properties, replaced all the card objects with a case class

--- fp/src/main/scala/nuno/random_exercises/CardTrick.scala
+++ fp/src/main/scala/nuno/random_exercises/CardTrick.scala
@@ -24,111 +24,57 @@ case object Hearts extends Hearts
 case object Clubs extends Clubs
 case object Diamonds extends Diamonds
 
-sealed trait Card extends Suit {
+sealed trait CardRank {
   def order: Int
 }
 
-trait Ace extends Card {
+case object Ace extends CardRank {
   val order = 13
 }
-trait Two extends Card {
+case object Two extends CardRank {
   val order = 1
 }
-trait Three extends Card {
+case object Three extends CardRank {
   val order = 2
 }
-trait Four extends Card {
+case object Four extends CardRank {
   val order = 3
 }
-trait Five extends Card {
+case object Five extends CardRank {
   val order = 4
 }
-trait Six extends Card {
+case object Six extends CardRank {
   val order = 5
 }
-trait Seven extends Card {
+case object Seven extends CardRank {
   val order = 6
 }
-trait Eight extends Card {
+case object Eight extends CardRank {
   val order = 7
 }
-trait Nine extends Card {
+case object Nine extends CardRank {
   val order = 8
 }
-trait Ten extends Card {
+case object Ten extends CardRank {
   val order = 9
 }
-trait Jack extends Card {
+case object Jack extends CardRank {
   val order = 10
 }
-trait Queen extends Card {
+case object Queen extends CardRank {
   val order = 11
 }
-trait King extends Card {
+case object King extends CardRank {
   val order = 12
 }
 
-case object AceSpades extends Ace with Spades
-case object TwoSpades extends Two with Spades
-case object ThreeSpades extends Three with Spades
-case object FourSpades extends Four with Spades
-case object FiveSpades extends Five with Spades
-case object SixSpades extends Six with Spades
-case object SevenSpades extends Seven with Spades
-case object EightSpades extends Eight with Spades
-case object NineSpades extends Nine with Spades
-case object TenSpades extends Ten with Spades
-case object JackSpades extends Jack with Spades
-case object QueenSpades extends Queen with Spades
-case object KingSpades extends King with Spades
-
-case object AceDiamonds extends Ace with Diamonds
-case object TwoDiamonds extends Two with Diamonds
-case object ThreeDiamonds extends Three with Diamonds
-case object FourDiamonds extends Four with Diamonds
-case object FiveDiamonds extends Five with Diamonds
-case object SixDiamonds extends Six with Diamonds
-case object SevenDiamonds extends Seven with Diamonds
-case object EightDiamonds extends Eight with Diamonds
-case object NineDiamonds extends Nine with Diamonds
-case object TenDiamonds extends Ten with Diamonds
-case object JackDiamonds extends Jack with Diamonds
-case object QueenDiamonds extends Queen with Diamonds
-case object KingDiamonds extends King with Diamonds
-
-case object AceClubs extends Ace with Clubs
-case object TwoClubs extends Two with Clubs
-case object ThreeClubs extends Three with Clubs
-case object FourClubs extends Four with Clubs
-case object FiveClubs extends Five with Clubs
-case object SixClubs extends Six with Clubs
-case object SevenClubs extends Seven with Clubs
-case object EightClubs extends Eight with Clubs
-case object NineClubs extends Nine with Clubs
-case object TenClubs extends Ten with Clubs
-case object JackClubs extends Jack with Clubs
-case object QueenClubs extends Queen with Clubs
-case object KingClubs extends King with Clubs
-
-case object AceHearts extends Ace with Hearts
-case object TwoHearts extends Two with Hearts
-case object ThreeHearts extends Three with Hearts
-case object FourHearts extends Four with Hearts
-case object FiveHearts extends Five with Hearts
-case object SixHearts extends Six with Hearts
-case object SevenHearts extends Seven with Hearts
-case object EightHearts extends Eight with Hearts
-case object NineHearts extends Nine with Hearts
-case object TenHearts extends Ten with Hearts
-case object JackHearts extends Jack with Hearts
-case object QueenHearts extends Queen with Hearts
-case object KingHearts extends King with Hearts
+case class Card(rank: CardRank, suit: Suit)
 
 object CardTrick {
   def mkWinner(trump: Option[Suit]): Seq[Card] => Card = seq =>
     seq.foldLeft(None: Option[Card]) {
       case (None, c) => Some(c)
-      case (Some(c1), c2) if c1.suit == c2.suit && c1.order > c2.order => Some(c1)
+      case (Some(c1), c2) if c1.suit == c2.suit && c1.rank.order > c2.rank.order => Some(c1)
       case (Some(c1), c2) if c1.suit == c2.suit || trump.contains(c2.suit) => Some(c2)
       case (Some(c1), _) => Some(c1)
     } get
--- fp/src/test/scala/nuno/random_exercises/CardTrickSpec.scala
+++ fp/src/test/scala/nuno/random_exercises/CardTrickSpec.scala
@@ -6,23 +6,49 @@ import org.scalatest.prop.PropertyChecks
 import org.scalatest.{Matchers, PropSpec}
 
 class CardTrickSpec extends PropSpec with PropertyChecks with Matchers {
-  val cards = Gen.oneOf(AceSpades, TwoSpades, ThreeSpades, FourSpades, FiveSpades, SixSpades, SevenSpades, EightSpades, NineSpades, TenSpades, JackSpades, QueenSpades, KingSpades,
-    AceHearts, TwoHearts, ThreeHearts, FourHearts, FiveHearts, SixHearts, SevenHearts, EightHearts, NineHearts, TenHearts, JackHearts, QueenHearts, KingHearts,
-    AceClubs, TwoClubs, ThreeClubs, FourClubs, FiveClubs, SixClubs, SevenClubs, EightClubs, NineClubs, TenClubs, JackClubs, QueenClubs, KingClubs,
-    AceDiamonds, TwoDiamonds, ThreeDiamonds, FourDiamonds, FiveDiamonds, SixDiamonds, SevenDiamonds, EightDiamonds, NineDiamonds, TenDiamonds, JackDiamonds, QueenDiamonds, KingDiamonds)
+  val suits = Gen.oneOf(Spades, Clubs, Hearts, Diamonds)
+  val ranks = Gen.oneOf(Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King)
+  val cards = for {
+    rank <- ranks
+    suit <- suits
+  } yield Card(rank, suit)
   val atLeastOneCard = Gen.nonEmptyContainerOf[Seq, Card](cards)
-  val trump = Gen.option(Gen.oneOf(Spades, Clubs, Hearts, Diamonds))
-  val trumpAndCards = Gen.zip(trump, atLeastOneCard)
 
-  implicit override val generatorDrivenConfig = PropertyCheckConfig(minSize = 4, maxSize = 20)
+  val trumpAndCardsWithTrumpCards = for {
+    s <- suits
+    cs <- atLeastOneCard if cs.exists(_.suit == s)
+  } yield (s, cs)
+
+  val trumpAndCardsWithoutTrumpCards = for {
+    s <- suits
+    cs <- atLeastOneCard if cs.forall(_.suit != s)
+  } yield (s, cs)
+
+  val NoTrumpAndCards = for {
+    cs <- atLeastOneCard
+    s <- Gen.const(None)
+  } yield (s, cs)
+
+  implicit override val generatorDrivenConfig = PropertyCheckConfig(minSize = 2, maxSize = 10)
   implicit val ordering = new Ordering[Card] {
-    override def compare(x: Card, y: Card): Int = x.order.compareTo(y.order)
+    override def compare(x: Card, y: Card): Int = x.rank.order.compareTo(y.rank.order)
   }
 
-  property("the winner's card is the biggest card of the trump suit or lead suit") {
-    forAll(trumpAndCards) {
-      case (suit@Some(s), cs) if cs.exists(_.suit == s) => cs.filter(_.suit == s).max shouldBe mkWinner(suit)(cs)
-      case (_, cs) => cs.filter(_.suit == cs.head.suit).max shouldBe mkWinner(None)(cs)
+  property("the winner's card is the biggest card of the trump suit") {
+    forAll(trumpAndCardsWithTrumpCards) {
+      case (suit, cs) => cs.filter(_.suit == suit).max shouldBe mkWinner(Some(suit))(cs)
+    }
+  }
+
+  property("the winner's card is the biggest card of the lead suit when no cards in the trump suit") {
+    forAll(trumpAndCardsWithoutTrumpCards) {
+      case (suit, cs) => cs.filter(_.suit == cs.head.suit).max shouldBe mkWinner(Some(suit))(cs)
+    }
+  }
+
+  property("the winner's card is the biggest card of the lead suit when no trump") {
+    forAll(NoTrumpAndCards) {
+      case (None, cs) => cs.filter(_.suit == cs.head.suit).max shouldBe mkWinner(None)(cs)
     }
   }
 }
